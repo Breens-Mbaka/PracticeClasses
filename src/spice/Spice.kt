9091db3739e518c283a8effcd27d6242b19d1c4e
Breens <breensthomas7353@gmail.com>
2020-10-14 08:02:46 +0300

    Created a list of objects and gave arguments of different spices

--- src/spice/Spice.kt
+++ src/spice/Spice.kt
@@ -1,13 +1,30 @@
 package spice
 
 fun main() {
-    val spice = Spice()
-    println("The spice name is ${spice.name} and degree of spiciness is ${spice.heat}")
+    val spice = Spice("Cayenne", spiciness= "Hot")
+    println(spice.heat)
 }
 
-class Spice {
-    val name: String = "Curry"
-    val spiciness: String = "Mild"
+val spices1 = listOf(
+Spice("curry", "mild"),
+Spice("pepper", "medium"),
+Spice("cayenne", "spicy"),
+Spice("ginger", "mild"),
+Spice("red curry", "medium"),
+Spice("green curry", "mild"),
+Spice("hot pepper", "extremely spicy")
+)
+
+class Spice(val name: String, val spiciness: String = "mild") {
     val heat: Int
-    get() {return 5}
+    get() {
+        return when (spiciness) {
+            "Flat" -> 2
+            "Mild" -> 5
+            "Hot" -> 8
+            "Very hot" -> 10
+            "Extra hot" -> 12
+            else -> 0
+        }
+    }
 }
