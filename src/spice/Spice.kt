4ee9c7beaacf09bd1c0a57d682f00ecc5709698e
Breens <breensthomas7353@gmail.com>
2020-10-14 09:02:36 +0300

    How interfaces work and how they help reduce subclassing.Will have to practice since this seems an important feature.

--- src/spice/Spice.kt
+++ src/spice/Spice.kt
@@ -1,21 +1,28 @@
 package spice
 
-fun main() {
-    val spice = Spice("Curry", spiciness = "Hot")
-    println("The ${spice.name} its ${spice.spiciness} and its level of spiceness is ${spice.heat}")
+abstract class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor): SpiceColor by color {
+    abstract fun prepareSpice()
 }
 
-
-class Spice(val name: String, val spiciness: String = "mild") {
-    val heat: Int
-    get() {
-        return when (spiciness) {
-            "Flat" -> 2
-            "Mild" -> 5
-            "Hot" -> 8
-            "Very hot" -> 10
-            "Extra hot" -> 12
-            else -> 0
-        }
+class Curry(name: String,spiciness: String,color: SpiceColor = YellowSpiceColor): Spice(name, spiciness,color), Grinder {
+    override fun grind() {
+        println("Curry is made of mixtures which are grind")
     }
+
+    override fun prepareSpice() {
+        grind()
+    }
+}
+
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: String
+}
+
+object YellowSpiceColor: SpiceColor{
+    override val color: String
+        get() = "Yellow"
 }
